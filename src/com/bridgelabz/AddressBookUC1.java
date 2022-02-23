0d45e33fc26561da454d1c2e1a15efdf9945b19b
MounikaAlloju <mounika.alloju@gmail.com>
2022-02-23 12:00:40 +0530

    UC1 added with Address book and Persons class

---
+++ src/com/bridgelabz/AddressBookUC1.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+/**
+ * @author mounika
+ *
+ */
+public class AddressBookUC1 {
+
+
+
+ArrayList<Persons> persons;
+
+public AddressBookUC1() {
+	persons=new ArrayList<Persons>();
+	
+}
+
+public static void main(String[] args) {
+	System.out.println("Welcome to Address Book System ");
+}
+}
